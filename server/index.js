8ea57f6d3ad3a3cc7aef370dc4ca181d25036653
NadiaPia <206i417@gmail.com>
2023-02-26 23:11:50 -0800

    Add /register post handler

--- server/index.js
+++ server/index.js
@@ -1,6 +1,35 @@
 const express = require("express");
 const app = express();
-const db = require("./models")
+const db = require("./models");
+const { Users } = require("./models");
+const bcrypt = require('bcrypt');
+
+app.use(express.json());
+
+app.post("/register", (req, res) => {
+    const {username, password} = req.body;
+    bcrypt.hash(password, 10).then((hash) => {    //npm install bcrypt
+        Users.create({
+            username: username,
+            password: hash,
+        }).then(() => {
+            res.json("USER REGISTRATED!!!");
+        }).catch((error) => {
+            if (err) {
+                res.status(400).json({error: err});
+            }
+        });
+    });
+
+});
+
+app.post("/login", (req, res) => {
+    res.json("login");
+})
+
+app.get("/profile", (req, res) => {
+    res.json("profile");
+});
 
 
 db.sequelize.sync().then(() => {
